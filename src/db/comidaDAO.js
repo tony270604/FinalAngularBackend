8767a9ef2ece83a6832055000c273775e0cf456d
tony270604 <tony270604@gmail.com>
2024-12-06 23:18:31 -0500

    Connvertir la imagen en base64

--- src/db/comidaDAO.js
+++ src/db/comidaDAO.js
@@ -1,14 +1,25 @@
 //Importar la funcion conexcion
 const {getConexion}=require('./conexion');
 
-function listarComida(){
-    return new Promise((resolve, reject)=>{
-        const conexion= getConexion();
-        conexion.query(`select * from comida`,(error,result)=>{
-            return error ? reject(error):resolve(result);
+const listarComida = () => {
+    return new Promise((resolve, reject) => {
+        const conexion = getConexion();
+        conexion.query(`SELECT * FROM comida`, (error, result) => {
+            if (error) {
+                return reject(error);
+            }
+            // Convertir los campos binarios (BLOB) a Base64
+            const processedResult = result.map(comida => {
+                if (comida.img1_com) {
+                    comida.img1_com = `data:image/png;base64,${comida.img1_com.toString('base64')}`;
+                }
+                return comida;
+            });
+
+            resolve(processedResult);
         });
-    })
-}
+    });
+};
 
 module.exports={
     listarComida,
